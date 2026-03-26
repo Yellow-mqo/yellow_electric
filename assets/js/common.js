83bac01b636db63efd3030ab1e82315d9dbda790
Yellow.mqo <sakusakunokoromo2351000@gmail.com>
2026-03-26 14:25:53 +0900

    Update common.js

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,7 +1,5 @@
 async function loadHeader() {
     const response = await fetch('header.html');
-    const text = await response.json(); // もしくは .text()
-    // 実際には text() でHTMLとして流し込む
     const html = await response.text();
     document.getElementById('header-common').innerHTML = html;
 }
@@ -9,8 +7,6 @@ loadHeader();
 
 async function loadFooter() {
     const response = await fetch('footer.html');
-    const text = await response.json(); // もしくは .text()
-    // 実際には text() でHTMLとして流し込む
     const html = await response.text();
     document.getElementById('footer-common').innerHTML = html;
 }
