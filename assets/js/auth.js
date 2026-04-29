c389327cf911e3e0a347a4b582595ef44e3aa9ed
Yellow.mqo <sakusakunokoromo2351000@gmail.com>
2026-04-29 23:17:07 +0900

    a

--- assets/js/auth.js
+++
@@ -1,9 +0,0 @@
-function unlock() {
-    const pass = document.getElementById('pass-input').value;
-    if (pass === "Shin-Hakushima") {
-        document.getElementById('password-screen').style.display = 'none';
-        document.getElementById('secret-content').style.display = 'block';
-    } else {
-        document.getElementById('error-msg').style.display = 'block';
-    }
-}
